a5c159a7a0a42bd1b7cad92f2324fc40c31dde90
Neengash <pepmanas93@gmail.com>
2022-07-03 10:06:12 +0200

    Basic Activity Files

---
+++ Ordinador/install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+mkdir /var/log/dcultural
+cp srv1.log /var/log/dcultural/
+cp srv2.log /var/log/dcultural/
+cp srv3.log /var/log/dcultural/
+cp srv4.log /var/log/dcultural/
+cp complet.jpeg /var/log/dcultural/
+
+cp serverStatus /bin
+cp serverDeploy /bin
+cp serverLogs /bin
